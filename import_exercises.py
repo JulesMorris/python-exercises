c4d7639ccb473d0d1339c8f99b73f2c028b7f354
Julessa Morris <julessa.morris@gmail.com>
2022-04-16 15:12:06 -0400

    import exercises commit

---
+++ import_exercises.py
@@ -0,0 +1,17 @@
+#1) a)
+
+#copy pasted from terminal:
+
+# >>> import functions_exercises
+# >>> functions_exercises.is_vowel('e')  
+# True
+# >>> 
+
+# b) use from function to calculate tip
+
+from functions_exercises import calculate_tip2 #only works in computer terminal using "python {filename}" from python-exercises folder
+calculate_tip2(.25, 200)
+print(calculate_tip2(.25, 200)) # $50
+
+# 3) 
+
